8378bb044f23c26c15d2ae52b744dfe5e6742214
semiha <semiha.beyazkilic@gmail.com>
2023-05-27 22:56:29 +0200

    FIX: mariadb, nginx UP: wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,16 +2,18 @@
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';"
+sleep 3
 
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD
 
-mysql -e "GRANT ALL PRIVILEDGES ON '${MYSQL_DATABASE}' TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp_cli.sh
+++ srcs/requirements/wordpress/tools/wp_cli.sh
@@ -1,13 +1,47 @@
 #!/bin/sh
 
-FILE=/var/www/html/wordpress/wp-config.php
+FILE=/var/www/wordpress/wp-config.php
+cd /var/www/wordpress
 
-sleep 10
+if test ! -f "$FILE"; then
+	
+	#wp-cli install
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
 
-if test -f "$FILE"; then
-	wp config create --allow-root \
+	#sleep to allow mariadb time to setup
+	sleep 5
+
+	#wordpress download and unzip
+	wp core download --allow-root
+
+	#database setup
+	wp config create \
 		--dbname=$MYSQL_DATABASE \
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
-		--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+		--dbhost=mariadb:3306 \
+		--allow-root
+	
+	#webpage setup
+	wp core install \
+		--url=$DOMAIN_NAME \
+		--title="Semiha's Inception" \
+		--admin_user=$WP_ADMIN_USR \
+		--admin_password=$WP_ADMIN_PSSWD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--allow-root
+	
+	#user setup
+	wp create user \
+		$WP_USER_USR \
+		$WP_USER_MAIL \
+		--user_pass=$WP_USER_PSSWD \
+		--role=$WP_USER_ROLE \
+		--porcelain \
+		-allow-root
+
 fi
+
+/usr/sbin/php-fpm7.3 -F
